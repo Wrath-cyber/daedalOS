94a05ff7b787fa95ed03cd8db1d684cc4fdbd62b
Owen <73003857+Wrath-cyber@users.noreply.github.com>
2022-03-17 12:11:48 -0700

    Add files via upload

---
+++ public/Users/Public/Desktop/For Wurtle/APLineTest.java
@@ -0,0 +1,9 @@
+public class APLineTest {
+    public static void main(String[] args) {
+        APLine line1 = new APLine(5,4,-17);
+        double slope1 = line1.getSlope();
+        System.out.println(slope1);
+        System.out.println(line1.isOnLine(5,-2));
+
+    } 
+}
