1ba0c298a27b507107027d19d9db88effd56d31f
Owen <73003857+Wrath-cyber@users.noreply.github.com>
2022-03-18 12:03:54 -0700

    Add files via upload

---
+++ public/Users/Public/Desktop/For Wurtle/GameSpinnerTest.java
@@ -0,0 +1,30 @@
+public class GameSpinnerTest {
+    public static void main(String[] args){
+        GameSpinner g = new GameSpinner(4);
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+        System.out.println("Spin(): " + g.spin());
+        System.out.println("Current run #" + g.currentRun());
+    }
+}
